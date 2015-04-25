39b45f2d1ffc2e1d36549026350eeb76a1e7f340
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-04-25 18:59:57 -0300

    More accurate chart. now showing exactly the moment when PC changes. Also showing moments when PC is not initialized

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,35 +2,51 @@ class Track
 	include Mongoid::Document
 	include Mongoid::Timestamps
 	field :status, type: Mongoid::Boolean
-	# scope :between, ->(begin_date, end_date) {where(:created_at.gte => begin_date, :created_at.lte => end_date.to_datetime.end_of_day).group_by {|d| d.created_at.strftime("%d/%m/%Y")}}
-
+	
 	scope :between, ->(begin_date, end_date){where(:created_at.gte => begin_date.to_datetime, :created_at.lte => end_date.to_datetime.end_of_day)}
 	scope :for_this, ->(date){where(:created_at.gte => date.to_datetime, :created_at.lte => date.to_datetime.end_of_day)}
 	
 	def self.init_days
 		days = Hash.new
 		start_date = self.first.created_at.to_date
-		end_date = self.last.created_at.to_date
-		last = false
-	  (start_date..end_date).each do |date| 
+		end_date = DateTime.now
+		last = nil
+	  (start_date..end_date).each do |date|
 		  day = Hash.new
 			self.for_this(date).each do |track|
+				time_in_seconds = track.created_at.strftime("%M").to_i*60 + track.created_at.strftime("%S").to_i
 				if day[track.created_at.strftime("%H").to_i].nil?
-					day[track.created_at.strftime("%H").to_i] = [track.status]
+					day[track.created_at.strftime("%H").to_i] = {time_in_seconds => track.status}
 				else
-					day[track.created_at.strftime("%H").to_i] << track.status
+					day[track.created_at.strftime("%H").to_i].merge!({time_in_seconds => track.status})
 				end
 			end
-	  	24.times do |hour|
+			day = day.sort.to_h
+
+		 	24.times do |hour|
 	  		if day[hour].nil?
-	  			day.merge!({hour => [last]})
+	  			day.merge!({hour => {0 => last}})
 	  		else
-	  			last = day[hour].last
+	  			day[hour][0] = last
+	  			day[hour] = day[hour].sort.to_h
+		  		last = day[hour].values.last
 	  		end
 	  	end
-			days.merge!({date.strftime("%d-%m")=>day.sort.to_h}) 
+	  	if date == Date.today
+				24.times do |hour|
+					right_now = DateTime.now
+					if hour == DateTime.now.strftime('%H').to_i
+						time_in_seconds = right_now.strftime("%M").to_i*60 + right_now.strftime("%S").to_i
+						day[hour] = {0=>last, time_in_seconds => nil }
+					end
+					if hour > DateTime.now.strftime('%H').to_i
+						day[hour] = {0 => nil}
+					end
+				end
+			end
+			days.merge!({date.strftime("%a, %d %b")=>day.sort.to_h}) 
 	  end 
-	  days		
+	  days
 	end
 
 end
