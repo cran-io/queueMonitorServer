f11654bc091f5fefd6ba408ec9cbd8af0232774f
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-04-27 14:15:03 -0300

    Tests

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe TracksController, type: :controller do
     end
   end
 
+  describe "POST #create" do 
+  	context "with valid format" do
+  		it "should create a track" do
+  			expect{
+	  			post :create, :track => {:status => false }
+  			}.to change(Track, :count).by(+1)
+  			expect(response).to have_http_status(:success)
+  		end
+
+  		it "should not create a track" do
+  			expect{
+	  			post :create, :track => {:status => nil }
+  			}.to change(Track, :count).by(0)
+  			expect(response).not_to have_http_status(:success)
+  		end
+  	end
+  	context "with invalid format" do
+  		it "tries to create a track" do
+  			expect{
+	  			post :create, :track => {:foobar => false }
+  			}.to change(Track, :count).by(0)
+  			expect(response).not_to have_http_status(:success)
+  		end
+  	end
+  end
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 	describe "validations" do
+		it {should validate_inclusion_of(:status).in_array([true,false])}
+	end
+
+	describe "methods" do
+		describe "init days" do
+			context "with or without tracks" do
+				it "should not be empty" do
+					expect(Track.init_days(Date.today-10, Date.today)).to_not be_empty
+				end
+			end
+		end
+	end
 end
